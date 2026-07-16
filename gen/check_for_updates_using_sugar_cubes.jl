e1807ae25c305b7eb821305c72a1569577ec722b
WooKyoung Noh <wookay.noh@gmail.com>
2026-07-16 09:12:51 +0900

    gen/check_for_updates codex

--- gen/check_for_updates_using_sugar_cubes.jl
+++ gen/check_for_updates_using_sugar_cubes.jl
@@ -7,9 +7,9 @@ using SugarCubes: code_block_with, has_diff
 # https://github.com/wookay/SugarCubes.jl
 
 function check_the_code_block_diff(src_path::String,
-                                   src_signature::Expr,
+                                   src_signature::Union{Nothing, Expr},
                                    dest_path::String,
-                                   dest_signature::Expr)
+                                   dest_signature::Union{Nothing, Expr})
     printstyled(stdout, "✔ ", color = :blue)
     print(stdout, " ", basename(src_path), " ")
     src_filepath = normpath(@__DIR__, "..", src_path)
@@ -17,19 +17,23 @@ function check_the_code_block_diff(src_path::String,
     @test isfile(src_filepath)
     @test isfile(dest_filepath)
     src_block = code_block_with(; filepath = src_filepath, signature = src_signature)
-    (depth, kind, sig) = src_block.signature.layers[end]
-    printstyled(stdout, sig.args[1], color = :cyan)
+    if src_block.signature !== nothing
+        (depth, kind, sig) = src_block.signature.layers[end]
+        printstyled(stdout, sig.args[1], color = :cyan)
+    end
     dest_block = code_block_with(; filepath = dest_filepath, signature = dest_signature)
     @test has_diff(src_block, dest_block) === false
     println(stdout)
 end
 
 if VERSION >= v"1.14-DEV"
+
+f = :(function show_limited(io::IO, mime::MIME, x) end)
 check_the_code_block_diff(
     "src/REPL.jl",
     :(if VERSION >= v"1.13.0-DEV.620" elseif VERSION >= v"1.11.0" function show_limited(io::IO, mime::MIME, x) end end),
     "sources/stdlib/REPL/src/REPL.jl",
-    :(module REPL function show_limited(io::IO, mime::MIME, x) end end)
+    :(module REPL $f end)
 )
 
 check_the_code_block_diff(
@@ -39,10 +43,22 @@ check_the_code_block_diff(
     :(module REPL function display(d::REPLDisplay, mime::MIME"text/plain", x) end end)
 )
 
+f = :(function show_repl(io::IO, mime::MIME"text/plain", c::AbstractChar) end)
 check_the_code_block_diff(
     "src/REPL.jl",
     :(if v"1.13.0-DEV.620" > VERSION >= v"1.11.0" function show_repl(io::IO, mime::MIME"text/plain", c::AbstractChar) end end),
     "sources/stdlib/REPL/src/REPL.jl",
-    :(module REPL function show_repl(io::IO, mime::MIME"text/plain", c::AbstractChar) end end)
+    :(module REPL $f end)
 )
+
+check_the_code_block_diff(
+    "gen/codex/emoji_symbols.jl", nothing,
+    "sources/stdlib/REPL/src/emoji_symbols.jl", nothing
+)
+
+check_the_code_block_diff(
+    "gen/codex/latex_symbols.jl", nothing,
+    "sources/stdlib/REPL/src/latex_symbols.jl", nothing
+)
+
 end # if
